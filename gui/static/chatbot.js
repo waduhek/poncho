512182341e83e3bcece3df96de4a6043d2065bd2
Ryan Noronha <ryannor56a@gmail.com>
2019-03-30 17:11:23 +0530

    Chat messages now are sent when enter key is pressed

--- gui/static/chatbot.js
+++ gui/static/chatbot.js
@@ -1,18 +1,33 @@
-$(document).ready(function(){
-	$('#submitmsg').click(function(){
+function updateScroll() {
+	var chatbox = document.getElementById('chatbox');
+	chatbox.scrollTop = chatbox.scrollHeight;
+}
 
-		//console.log('clicked')
+function chat() {
+	setInterval(updateScroll, 1);
+		
+	if ($('#usermsg').val() != "") {
 		var input = $('#usermsg').val();
 		console.log(input);
 
-		$('#chatbox').append('<p style="font-size:20px"><span>You: </span>'+input+'</p>');
+		$('#chatbox').append('<p style="font-size:18px"><span>You: </span>' + input + '</p>');
 		$('#usermsg').val("");
 
 		$.post("/msg", {
-			data:input
+			data: input
 		}, function(data) {
 			console.log(data);
-			$('#chatbox').append('<p style="font-size:20px"><span>Poncho: </span>'+data+'</p>');
+			$('#chatbox').append('<p style="font-size:18px"><span>Poncho: </span>' + data + '</p>');
 		});
-	});
+	}
+}
+
+function chatOnEnter(event) {
+	if (event.keyCode == 13) {
+		chat();
+	}
+}
+
+$(document).ready(function(){
+	$('#submitmsg').click(chat());
 });
